79e0e3b89f5e99b2a4916b810e0dc9b8bfabb45f
stevenmira <steven.mira23@hotmail.com>
2017-06-06 00:29:27 -0600

    Se añadio 1 CRUD más. total CRUD a la fecha 3 (tipo_persona, grado, seccion)

--- Escuela/app/Http/routes.php
+++ Escuela/app/Http/routes.php
@@ -17,3 +17,4 @@ Route::get('/', function () {
 
 Route::resource('datos/tipoResponsable','TipoResponsableController');
 Route::resource('detalle/grado','GradoController');
+Route::resource('detalle/seccion','SeccionController');
