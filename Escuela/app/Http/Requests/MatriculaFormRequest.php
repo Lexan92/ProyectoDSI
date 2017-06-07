d285103bc9fd8846b9c2aec48c1c23f18a83653f
stevenmira <steven.mira23@hotmail.com>
2017-06-06 22:57:56 -0600

    Se ha agregado los archivos model, request para las tablas (responsable, detalle_grado, detalle_pariente, matricula) y ademas el archivo controller para la tabla matricula.

---
+++ Escuela/app/Http/Requests/MatriculaFormRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Escuela\Http\Requests;
+
+use Escuela\Http\Requests\Request;
+
+class MatriculaFormRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nie'=>'required',
+            'iddetallegrado'=>'required',
+            'presentapartida'=>'required|max:50',
+            'certificadoprom'=>'required|max:50',
+            'presentafotos'=>'required|max:200',
+            'constanciaconducta'=>'required',
+            'educacioninicial'=>'required'
+            'fechamatricula'=>'required',
+            'repitegrado'=>'required',
+            'fotografia'=>'required',
+            #'estado'=>'required',
+        ];
+    }
+}
---
+++ Escuela/app/Matricula.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Escuela;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Matricula extends Model
+{
+    protected $table = 'matricula';
+
+    protected $primaryKey = 'id_matricula';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+    	'nie',
+    	'iddetallegrado'
+    	'presentapartida',
+    	'certificadoprom',
+    	'presentafotos',
+    	'constanciaconducta',
+    	'educacioninicial',
+    	'fechamatricula',
+    	'repitegrado',
+    	'fotografia',
+    	'estado'
+    ];
+
+    protected $guarded = [
+    ];
+}
