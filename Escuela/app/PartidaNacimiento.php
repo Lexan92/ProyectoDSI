591d3bdfda2dc01e0f014a61ec2d7f3490588e11
Lexan92 <luis_grennby92@hotmail.com>
2017-06-07 22:48:28 -0600

    PARTIDA NACIMIENTO
    creacion del modelo, request y controlador

---
+++ Escuela/app/PartidaNacimiento.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Escuela;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PartidaNacimiento extends Model
+{
+   protected $table = 'partidanacimiento';
+   protected $primaryKey = 'id_partida';
+   public $timestamps = false;
+   protected $fillable=[
+   			'nie',
+   			'folio',
+   			'libro',
+   			'copiapartida'
+   ];
+
+   protected $guarded = [
+    ];
+}
